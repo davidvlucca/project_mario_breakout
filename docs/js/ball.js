13b7e0f2b30d0b57a6453e7b13b5144af85893ec
David Lucca <133000227+davidvlucca@users.noreply.github.com>
2024-09-02 21:30:28 +0100

    Adjusting again the speed of the ball.js

--- docs/js/ball.js
+++ docs/js/ball.js
@@ -6,7 +6,7 @@ class Ball {
   this.width = width;
   this.height = height;
   this.imgSrc = imgSrc;
-  this.speed = 3;
+  this.speed = 2;
   this.directionX = -this.speed;
   this.directionY = this.speed;
   this.element = document.createElement("img");
